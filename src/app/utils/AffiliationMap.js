f92386ed7f6b4f4ae1997ab204f3579c2cec9d52
steemleo <52357015+steemleo@users.noreply.github.com>
2019-08-24 22:07:02 -0500

    Update AffiliationMap.js

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -1,25 +1,25 @@
 const map = {
  //SteemLeo Team
-    "khaleelkazi": "SteemLeo Founder",
-    "steem.leo": "Official SteemLeo",
-    "learn.leo": "Official SteemLeo",
-    "leo.voter": "Official SteemLeo",
-    "noleo4u": "SteemLeo Moderator",
-    "leo.curation": "Official SteemLeo",
-    "leo.bounties": "Official SteemLeo",
-    "leo.syndication": "Official SteemLeo",
+    khaleelkazi: 'SteemLeo Founder',
+    steem.leo: 'Official SteemLeo',
+    learn.leo: 'Official SteemLeo',
+    leo.voter: 'Official SteemLeo',
+    noleo4u: 'SteemLeo Moderator',
+    leo.curation: 'Official SteemLeo',
+    leo.bounties: 'Official SteemLeo',
+    leo.syndication: 'Official SteemLeo',
 
     //SteemLeo Curators
-    "gerber": "SteemLeo Curator",
-    "leothecat": "SteemLeo Curator",
-    "neal.leo": "SteemLeo Curator",
-    "scaredycatcurate": "SteemLeo Curator",
-    "scaredycatguide": "SteemLeo Curator",
-    "vxc": "SteemLeo Curator",
-    "vxc.leo": "SteemLeo Curator",
-    "schubes": "SteemLeo Curator",
-    "mindtrap": "SteemLeo Curator",
-    "mindtrap.leo": "SteemLeo Curator",
+    gerber: 'SteemLeo Curator',
+    leothecat: 'SteemLeo Curator',
+    neal.leo: 'SteemLeo Curator',
+    scaredycatcurate: 'SteemLeo Curator',
+    scaredycatguide: 'SteemLeo Curator',
+    vxc: 'SteemLeo Curator',
+    vxc.leo: 'SteemLeo Curator',
+    schubes: 'SteemLeo Curator',
+    mindtrap: 'SteemLeo Curator',
+    mindtrap.leo: 'SteemLeo Curator',
 */
 };
 
